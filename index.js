049ee4e3c07f55d2e7d00cbdc1d6b08e1ffd2acc
SallyBoBally <mesallyv@gmail.com>
2024-12-17 17:49:45 -0800

    part 3 confusion

--- index.js
+++ index.js
@@ -27,3 +27,11 @@ app.listen(port, () => {
     console.log(`Example app listening
 at http://localhost:${port}`)
 })
+
+const swaggerUi = require('swagger-ui-express');
+swaggerDocument = require('./swagger.json');
+app.use(
+'/api-docs',
+swaggerUi.serve,
+swaggerUi.setup(swaggerDocument)
+);

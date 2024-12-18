0dd1ba84a7477437e7379f08b2cddb9628bc859b
SallyBoBally <mesallyv@gmail.com>
2024-12-17 20:16:54 -0800

    added the rest

--- calculator-app/calculatorController.js
+++ calculator-app/calculatorController.js
@@ -1,14 +1,44 @@
 const addNumbers = (req, res) => {
     let number1 = parseInt(req.query.num1);
     let number2 = parseInt(req.query.num2);
-    let sum = number1 + number2
+    let sum = number1 + number2;
     console.log(sum)
     res.status(200)
     res.json({result:sum})
 }
 
+const subNumbers = (req, res) => {
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+    let sub = number1 - number2;
+    console.log(sub)
+    res.status(200)
+    res.json({result:sub})
+}
+
+const multiplyNumbers = (req, res) => {
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+    let multipy = number1 * number2;
+    console.log(multiply)
+    res.status(200)
+    res.json({result:multipy})
+}
+
+const divideNumbers = (req, res) => {
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+    let divide = number1 / number2;
+    console.log(divide)
+    res.status(200)
+    res.json({result:divide})
+}
+
 module.exports = {
-    addNumbers
+    addNumbers,
+    subNumbers,
+    multiplyNumbers,
+    divideNumbers
 }
 
 const Calculator =
